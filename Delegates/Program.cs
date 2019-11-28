34063304909b3317a3a95defe399dac0ce24862b
Marcos Marins <marcos-henrique2000@hotmail.com>
2019-11-28 08:49:55 -0300

    Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Delegates.Services;
+
+namespace Delegates
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+
+            double result = op(a, b); // or op.Invoke(a, b)
+            Console.WriteLine(result);
+        }
+    }
+}
